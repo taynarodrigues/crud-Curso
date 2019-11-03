7389e4c0209b43f6d65ec7b80b458bf83768be91
Tayna Rodrigues <taynajesus@gmail.com>
2019-11-03 17:18:47 -0500

    Http GET: listar registros

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,22 @@
+import { CursosService } from './../../cursos.service';
 import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.css']
+  styleUrls: ['./cursos-lista.component.css'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos: Curso[];
+
+  constructor(private service: CursosService) { }
 
   ngOnInit() {
+    this.service.list()
+    .subscribe(dados => this.cursos = dados);
   }
 
 }
